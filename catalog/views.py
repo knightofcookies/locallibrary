fad32ce3bbba700db63143ad327dc7160f14c994
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2022-04-17 21:36:50 +0530

    Bug fixes

--- catalog/views.py
+++ catalog/views.py
@@ -83,7 +83,7 @@ class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
 @permission_required('catalog.can_mark_returned', raise_exception=True)
 def renew_book_librarian(request, pk):
     """View function for renewing a specific BookInstance by librarian."""
-    book_instance = get_object_or_404(BookInstance, pk=pk)
+    book_instance = get_object_or_404(BookInstance, id=pk)
 
     # If this is a POST request then process the Form data
     if request.method == 'POST':
